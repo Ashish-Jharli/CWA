b051c6aa1d7a24d84514ed9a9aef4ae3c65f51d9
Ashish Jharli <aj7121725@gmail.com>
2024-10-28 09:42:21 +0530

    Add files via upload

---
+++ Print motion/script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    $('#printer-animation').mouseleave(function () {
+        $(this).removeClass('clicked');
+    }).dblclick(function () {
+        $(this).addClass('clicked').html($(this).html());
+    });
+});
